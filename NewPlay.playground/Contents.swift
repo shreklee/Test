8597deeab88631414f2567f9fa405f87da43d54c
Shrek <Shrek.W.Lee@newegg.com>
2017-08-08 17:34:34 +0800

    add playground
    add

---
+++ NewPlay.playground/Contents.swift
@@ -0,0 +1,73 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+var optionalName: String!
+var greeting: String? = " Hello"
+
+// if let 代码
+if let name = optionalName, let greet = greeting {
+    name + greet
+}
+
+// guard let 代码
+
+func answer(){
+    guard let name = optionalName, let greet = greeting  else {
+        return
+    }
+    name + greet
+}
+
+answer()
+
+protocol Container{
+    var ItemType:Int!{
+        get
+    }
+    var count:Int!{
+        get
+    }
+}
+
+protocol Equatable{
+    
+}
+
+func allItemsMatch<
+    C1: Container, C2: Container
+    where C1.ItemType == C2.ItemType, C1.ItemType: Equatable>
+    (someContainer: C1, anotherContainer: C2) -> Bool {
+    
+    // check that both containers contain the same number of items
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+    
+    // check each pair of items to see if they are equivalent
+    for i in 0...someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+    
+    // all items match, so return true
+    return true
+    
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+
+var arrayOfStrings = ["uno", "dos", "tres"]
+
+if allItemsMatch(stackOfStrings, arrayOfStrings) {
+    println("All items match.")
+} else {
+    println("Not all items match.")
+}
+// 输出 "All items match."
